ab995a7875dd10fe28f45c00b7519d640cdf6fe0
Gilberto Martins <gilbertomartins643@gmail.com>
2021-02-25 11:22:30 -0300

    creat movimento da cobra

--- script.js
+++ script.js
@@ -5,23 +5,47 @@ let box = 32; // define que o jogo vai ser de 32 px
 // olha minha cobra ;-------;
 let snake = [];
 snake[0] = {
-    x:8 * box ,
-    y:8 * box
+    x: 8 * box,
+    y: 8 * box
 }
 
+// direçoes
+let direction = "right";
 
 // essa funcion vai criar nosso backgrand 
-function criarBG(){
+function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);// vai desenha o campo do jogo
+    context.fillRect(0, 0, 16 * box, 16 * box);// vai desenha o campo do jogo
 }
 
-function criarCobrinha(){
-    for(i = 0; i <snake.length; i++){
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -=box;
+    if(direction == "up") snakeY -=box;
+    if(direction == "down") snakeY +=box;
+
+    snake.pop();
+
+    let newHeadr = {
+        x: snakeX,
+        Y: snakeY
+    }
+
+    snake.unshift(newHeadr, 100);
+
+}
+
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x , snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG();
-criarCobrinha();
+let jogo = setInterval(iniciarJogo, 100);
